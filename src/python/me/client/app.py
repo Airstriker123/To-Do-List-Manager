1c5713b175f575b83277392d0e63000352c24885
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-06-10 23:55:40 +1000

    Template for project
    Created a base template for project ig.

---
+++ src/python/me/client/app.py
@@ -0,0 +1,32 @@
+#modules preinstalled up here
+import os
+
+try:
+    #list modules here
+    pass
+except:
+    #include their install command here e.g os.system('pip install <module>')
+    dependencies = [
+        #list your module in string nga -_-
+        "module_here",
+        "",
+    ]
+    os.system(f"pip install {dependencies}")
+
+class ToDOList:
+    #class static variables here most likely will have them
+    def __init__(self
+                 ):
+        pass
+    #construct app here
+
+    @staticmethod
+    def main() -> None:
+        #public static void main ahh method -_-
+        pass
+
+#only exec if opened as script and not module (why would it be opened as a module idfk)
+if __name__ == "__main__":
+    ToDOList()
+    ToDOList.main()
+
