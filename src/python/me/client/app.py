e8659af52ee3577476330b89039322643d7f7516
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-06-11 15:20:05 +1000

    Added new shit
    :)

--- src/python/me/client/app.py
+++ src/python/me/client/app.py
@@ -3,19 +3,33 @@ import os
 import sys
 import time
 try:
-    #list modules here
     import logging
+   # from backend.main import *
+    from colorama import Fore, Back, Style
+    #list modules here
 except:
     #include their install command here e.g os.system('pip install <module>')
     dependencies = [
         #list your module in string 🥷 -_-
-        "logging",
+        "colorama>=0.4.6",
         "",
     ]
+    #install custom py modules using pip
     os.system(f"pip install {dependencies}")
+    import logging
+   # from backend.main import *
+    from colorama import Fore, Back, Style
+logging.basicConfig(level=logging.INFO)
+reset = Fore.RESET
+red = Fore.RED
+blue = Fore.BLUE
+lred = Fore.LIGHTRED_EX
+lblue = Fore.LIGHTBLUE_EX
+green = Fore.GREEN
 
-class ToDOList:
+class ToDoList:
     #class static variables here most likely will have them
+    # variables for colour printing
     def __init__(self
                  ):
         pass
@@ -28,7 +42,7 @@ class ToDOList:
 
 #only exec if opened as script and not module (why would it be opened as a module idfk)
 if __name__ == "__main__":
-    ToDOList()
-    ToDOList.main()
+    ToDoList()
+    ToDoList.main()
 
-print('Hello world!')
+logging.debug(f"{green}Hello world!")
