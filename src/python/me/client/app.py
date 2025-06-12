62ac185ce1e5fd1bff2c72633b1027f1a4036df8
Airstriker <114228623+Airstriker123@users.noreply.github.com>
2025-06-12 12:46:55 +1000

    Updated banner look
    gradient banner added

--- src/python/me/client/app.py
+++ src/python/me/client/app.py
@@ -8,13 +8,14 @@ try:
    # from backend.main import *
     from colorama import Fore, Back, Style
     from ui.ui import UserInterface
+    import fade
     #list modules here
 except:
     #include their install command here e.g os.system('pip install <module>')
     dependencies = [
         #list your module in string 🥷 -_-
         "colorama>=0.4.6",
-        "",
+        "fade",
     ]
     #install custom py modules using pip
     for module in dependencies:
@@ -45,9 +46,12 @@ class ToDoList:
 
     @staticmethod
     def main() -> None:
-        print(f"{green}{UserInterface().banner}\n{yellow}{UserInterface().task_menu}{reset}")
-
-
+        try:
+            banner = UserInterface().banner
+            gradient_banner = fade.fire(banner)
+            print(f"{green}{gradient_banner}\n{yellow}{UserInterface().task_menu}{reset}")
+        except:
+            pass
 
 #only exec if opened as script and not module (why would it be opened as a module idfk)
 if __name__ == "__main__":
